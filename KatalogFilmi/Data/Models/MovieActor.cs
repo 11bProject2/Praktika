aa52b862e81d20da2fad3227e84d09e38fdfe44f
RadoSfv <r.stefchev2017@pmg-pernik.com>
2021-07-09 11:03:57 +0300

    delete class1

---
+++ KatalogFilmi/Data/Models/MovieActor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KatalogFilmi
+{
+    public class MovieActor
+    {
+        public int MovieId { get; set; }
+        [Required]
+        [MaxLength(30)]
+        [MinLength(3)]
+        public Movie Movie { get; set; }
+        public int ActorId { get; set; }
+        [Required]
+        [MaxLength(30)]
+        [MinLength(3)]
+        public Person Actor { get; set; }
+    }
+}
---
+++ KatalogFilmi/KatalogFilmi/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace KatalogFilmi
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            
+        }
+    }
+}
